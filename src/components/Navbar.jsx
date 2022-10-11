307d7663d21f6ad31a2067606b3a9394b6562fc9
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-11 10:34:38 +0300

    Project structure cleanup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+       <div className="container nav__container">
+        
+        </div> 
+    </nav>
+  )
+}
+
+export default Navbar
