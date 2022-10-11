dbd23a0e657d572e22dfe68549c7a1fcaf826c73
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-11 13:35:31 +0300

    Added CSS styling for the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,44 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { links } from "../data";
 import { GoThreeBars } from "react-icons/go";
+import { MdClose} from "react-icons/md";
 
 import Logo from "../images/logo.png";
-import "./Navbar.css";
+
+import "./navbar.css";
 
 const Navbar = () => {
+  const [isNavShowing, setIsNavShowing] = useState(false);
+
+
   return (
     <nav>
       <div className="container nav__container">
-        <Link to="/" className="logo">
-          <img src={Logo} alt="logo" />
+        <Link to="/home" className="logo">
+          <img src={Logo} alt="Nav logo" />
         </Link>
-        <ul className="nav__links">
+        <ul
+          className={`nav__links ${isNavShowing ? "show__nav" : "hide__nav"}`}
+        >
           {links.map(({ name, path }, index) => {
             return (
-              <li>
-                <NavLink to={path}>{name}</NavLink>
+              <li key={index}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => (isActive ? "active-nav" : "")}
+                >
+                  {name}
+                </NavLink>
               </li>
             );
           })}
         </ul>
-        <button className="nav__toggle-btn">
-          <GoThreeBars/>
+        <button
+          className="nav__toggle-btn"
+          onClick={() => setIsNavShowing((prevState) => !prevState)}
+        >
+          {isNavShowing ?<MdClose/> : <GoThreeBars/> }
         </button>
       </div>
     </nav>
