1de15b7da14a2d92317638389ac813609fac4b50
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-11 11:02:21 +0300

    Added Navbar Markup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
-import './Navbar.css';
+import { Link, NavLink } from "react-router-dom";
+import { links } from "../data";
+import { GoThreeBars } from "react-icons/go";
+
+import Logo from "../images/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-       <div className="container nav__container">
-        
-        </div> 
+      <div className="container nav__container">
+        <Link to="/" className="logo">
+          <img src={Logo} alt="logo" />
+        </Link>
+        <ul className="nav__links">
+          {links.map(({ name, path }, index) => {
+            return (
+              <li>
+                <NavLink to={path}>{name}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <button className="nav__toggle-btn">
+          <GoThreeBars/>
+        </button>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
