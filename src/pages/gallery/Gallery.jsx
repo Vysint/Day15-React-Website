696ba78c6a178aa6cebe8ce77e32260aded0d577
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-12 23:45:39 +0300

    Added Gallery Page Markup and it's CSS styling

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,9 +1,33 @@
-import './Gallery.css';
+import Header from "../../components/Header";
+import HeaderImage from "../../images/headerbg3.jpg";
+import "./Gallery.css";
 
 const Gallery = () => {
+  const galleryLength = 15;
+  const images = [];
+
+  for (let i = 1; i <= galleryLength ; i++ ){
+    images.push(require(`../../images/gallery${i}.jpg`))
+  }
   return (
-    <div>Gallery</div>
-  )
-}
+    <>
+      <Header image={HeaderImage} title="Our Gallery">
+        Quia illo est delectus minima adipisci aspernatur animi natus nobis?
+        Sint nulla facere repellendus quasi.
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {
+            images.map((image, index) => {
+              return <article key = {index}>
+                <img src={image} alt={`Gallery ${index + 1}`} />
+              </article>
+            })
+          }
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Gallery;
