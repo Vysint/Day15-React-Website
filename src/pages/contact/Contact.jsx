49e6715270f390683e40c42e115c4f0e33a9fbae
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-12 23:24:29 +0300

    Added Markup for Contact Page and it's CSS styling

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,28 @@
-import './Contact.css';
+import Header from "../../components/Header";
+import HeaderImage from "../../images/headerbg2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <Header title="Get In Touch" image={HeaderImage}>
+        Dolor sit amet consectetur adipisicing elit. Mollitia unde minus quas
+        quam reprehenderit maxime.
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a href="mailto:support@myself.com" target="__blank" rel = "noreferrer noopener"><MdEmail/></a>
+            <a href="http://m.me/vintage" target="__blank" rel = "noreferrer noopener"><BsMessenger/></a>
+            <a href="https://wa.me/your number" target="__blank" rel = "noreferrer noopener"><IoLogoWhatsapp/></a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
